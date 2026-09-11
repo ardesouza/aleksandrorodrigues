da2c6382d43441ac0da53dd6025af568bf472ce2
Aleksandro Rodrigues <ar_desouza@hotmail.com>
2026-09-11 11:15:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,24 @@ const year = document.querySelector('#year');
 if (year) {
     year.textContent = new Date().getFullYear();
 }
+
+// ===============================
+// MENU MOBILE SANDUICHE
+// ===============================
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+    menuToggle.textContent =
+        menu.classList.contains('active') ? '✕' : '☰';
+});
+
+document.querySelectorAll('#menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active');
+        menuToggle.textContent = '☰';
+    });
+});
